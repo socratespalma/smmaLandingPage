03c37ca8f45507f6c640c3222b0e976de5c70314
Socrates <dubstepero10@gmail.com>
2021-02-12 21:41:19 -0600

    adding more changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,21 +2,34 @@ const menuBtn = document.querySelector(".menu-btn");
 const hamburger = document.querySelector(".menu-btn__burger");
 const nav = document.querySelector(".nav");
 const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav__item");
 
 let showMenu = false;
+
+let hideMenuOnTap = () => {
+  hamburger.classList.remove("open");
+  nav.classList.remove("open");
+  menuNav.classList.remove("open");
+  navItems.forEach((item) => item.classList.remove("open"));
+
+  showMenu = false;
+};
 
 let toggleMenu = () => {
   if (!showMenu) {
     hamburger.classList.add("open");
     nav.classList.add("open");
     menuNav.classList.add("open");
+    navItems.forEach((item) => item.classList.add("open"));
+
     showMenu = true;
   } else {
-    hamburger.classList.remove("open");
-    nav.classList.remove("open");
-    menuNav.classList.remove("open");
-    showMenu = false;
+    hideMenuOnTap();
   }
 };
 
 menuBtn.addEventListener("click", toggleMenu);
+
+for (const item of navItems) {
+  item.addEventListener("click", hideMenuOnTap);
+}
